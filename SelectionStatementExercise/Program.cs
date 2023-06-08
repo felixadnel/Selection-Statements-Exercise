2cb5b1a2d2915acaf3f62c1cb566a0e6b4700e4a
felix adnel <flixadnel@gmail.com>
2023-06-08 00:29:35 -0500

    initial commit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("welcome to the number guessing game!");
+            Console.WriteLine("see if you cn guess the nmber correctly!");
+            Console.WriteLine("what is the upper limit of the range number?");
+
+            var userResponse = (Console.ReadLine());
+            var upperLimit = int.Parse(userResponse);
+            var random = new Random();
+            var number = random.Next(1, 10);
+
+            Console.WriteLine("input your guess please");
+
+            var guess = int.Parse(Console.ReadLine());
+
+            if (guess > number)
+            {
+                Console.WriteLine("your guess is too high");
+            }
+
+            else if (guess < number)
+            {
+                Console.WriteLine("your guess is too low");
+            
+            } 
+            else 
+            {
+               Console.WriteLine("correct! Great job!");
+            
+            
+            }
         }
     }
 }
